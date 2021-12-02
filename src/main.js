b4c6d98170e481cc58a45c98657648bb681a1929
guofeng <guofeng@akulaku.com>
2021-12-02 18:49:16 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-plus'
+import 'element-plus/dist/index.css'
+createApp(App)
+  .use(ElementUI)
+  .mount('#app')
 
-createApp(App).mount('#app')
